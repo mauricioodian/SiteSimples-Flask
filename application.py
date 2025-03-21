0029ebb6b0243b60f8e07d6ad5108fd2630f7fc2
Mauricio de Oliveira Dian <mauricio.dian@outlook.com>
2025-03-21 16:02:25 -0300

    Tudo OK para Elastic Beanstalk

--- application.py
+++ application.py
@@ -8,30 +8,30 @@ from auth import auth
 # Carregar as variáveis do arquivo .env
 load_dotenv()
 
-app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
-app.register_blueprint(auth, url_prefix='/auth')  # Define um prefixo para as rotas
+application = Flask(__name__)
+application.secret_key = os.getenv('SECRET_KEY')
+application.register_blueprint(auth, url_prefix='/auth')  # Define um prefixo para as rotas
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/perfil')
+@application.route('/perfil')
 def profile():
     return render_template('perfil.html')
 
 
-@app.route('/termos')
+@application.route('/termos')
 def terms():
     return render_template('termos.html')
 
 
-@app.route('/politicas')
+@application.route('/politicas')
 def polices():
     return render_template('politicas.html')
 
 # Roda o servidor Flask
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(port=5000, debug=True)
